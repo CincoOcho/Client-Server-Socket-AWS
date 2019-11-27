5b375b91e7d3e4842d493a8d6457123af9e23c48
Luis Del toro <jldg.077@gmail.com>
2019-11-27 07:03:45 -0600

    Test in  client

--- src/finalsd/Client.java
+++ src/finalsd/Client.java
@@ -1,21 +1,61 @@
 package finalsd;
-import java.io.*;  
-import java.net.*; 
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 /**
  *
  * @author CincoOcho <///7>
  */
 public class Client {
-        public static void main(String[] args) {  
-		try{      
-			Socket s=new Socket("localhost",6666);  //IP , PORT CONFIG
-			DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-			dout.writeUTF("Hello Server");  
-			dout.flush();  
-			dout.close();  
-			s.close();  
-		}catch(Exception e){System.out.println(e);}  
-	} 
-    
+
+    public static String user, msg = "";
+
+    public static void main(String[] args) {
+        inputUser();
+        server();
+
+    }
+
+    public static void server() {
+
+        try {
+            Socket s = new Socket("localhost", 6666);  //IP , PORT CONFIG
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            dout.writeUTF("Hello Server");
+            dout.flush();
+            dout.close();
+            s.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void inputUser() {
+        int randomServer=0;
+        boolean msgOk = false;
+        String passOk="";
+        Scanner scan = new Scanner(System.in);
+        while (msgOk != true) {
+            System.out.print("Introduzca su nombre de usuario : ");
+            user = scan.nextLine();
+            System.out.print("Introduzca su mensaje a continuación: ");
+            msg = scan.nextLine();
+            System.out.println("Desea enviar el mensaje ?");
+            System.out.print("Si(s) / No(n) ? -> ");
+            passOk = scan.nextLine();
+            if ("S".equals(passOk)) {
+                System.out.println("Se enviará el mensaje al servidor numero"+randomServer+"");
+                msgOk=true;
+                
+            }
+            else{
+                System.out.println("No se enviará nada al servidor !");
+                msgOk=false;
+            }
+        }
+
+    }
+
 }
